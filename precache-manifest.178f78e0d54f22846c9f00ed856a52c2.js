fc08a2f7a2ff50e10f976ee1a2216f92d7054fb4
Jack Deadman <jdeadman1@sheffield.ac.uk>
2019-11-17 23:03:29 +0000

    Updates

---
+++ precache-manifest.178f78e0d54f22846c9f00ed856a52c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6193afb73c1be823fa1a0f32e58e2b",
+    "url": "/use-mouse-drag/index.html"
+  },
+  {
+    "revision": "62fd2d8959f05b276bf5",
+    "url": "/use-mouse-drag/static/css/main.1612d9f0.chunk.css"
+  },
+  {
+    "revision": "450bef33fed585573ce9",
+    "url": "/use-mouse-drag/static/js/2.bbbe7ac9.chunk.js"
+  },
+  {
+    "revision": "62fd2d8959f05b276bf5",
+    "url": "/use-mouse-drag/static/js/main.8285c6e5.chunk.js"
+  },
+  {
+    "revision": "13a7082821e818860f52",
+    "url": "/use-mouse-drag/static/js/runtime-main.07ff39a9.js"
+  }
+]);
